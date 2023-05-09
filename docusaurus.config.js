05f9ef779ed6ec5858d23807f8d11b3a4eea9a3c
Dominik Peters <mail@dominik-peters.de>
2023-05-09 12:08:18 +0200

    only load katex.min.css when needed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -169,15 +169,15 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
-    stylesheets: [
-      {
-        href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-        type: 'text/css',
-        integrity:
-          'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-        crossorigin: 'anonymous',
-      },
-    ],
+    // stylesheets: [
+    //   {
+    //     href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+    //     type: 'text/css',
+    //     integrity:
+    //       'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+    //     crossorigin: 'anonymous',
+    //   },
+    // ],
     plugins : [
       async function faviconInjector(context, options) {
         return {
