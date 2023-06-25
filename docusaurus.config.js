9027d97dc954ac12ce8e7e94d1dedfd4df6f0858
Dominik Peters <mail@dominik-peters.de>
2023-06-25 19:36:07 +0200

    activate french

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['de', 'en', 'pl'], //, 'fr'
+    locales: ['de', 'en', 'pl', 'fr']
   },
 
   presets: [
